9ae01bf426dba35bae7bf4e5ff99b42244927e60
Soffmcm <sofiamcm.school@gmail.com>
2026-02-06 21:43:48 +0100

    Add question loading functionality to quiz

--- script.js
+++ script.js
@@ -4,6 +4,75 @@ function nextSlide() {
   document.getElementById(`slide${current}`).classList.remove("active");
   current++;
   document.getElementById(`slide${current}`).classList.add("active");
+
+  if (current === 2) {
+    loadQuestion();
+  }
+}
+
+
+const questions = [
+  {
+    text: "Who loves who more?",
+    answers: [
+      { text: "You love me more", correct: false },
+      { text: "I love you more", correct: true }
+    ]
+  },
+  {
+    text: "Who's stronger",
+    answers: [
+      { text: "You", correct: false },
+      { text: "Me", correct: true }
+    ]
+  },
+  {
+    text: "Who’s cuter?",
+    answers: [
+      { text: "You", correct: true },
+      { text: "Me", correct: false}
+    ]
+  },
+   {
+    text: "Who’s tougher, taller, cooler, and more non-chalante?",
+    answers: [
+      { text: "You", correct: false },
+      { text: "Me", correct: true}
+    ]
+  }
+];
+
+let questionIndex = 0;
+
+
+function loadQuestion() {
+  document.getElementById("questionText").innerText =
+    questions[questionIndex].text;
+
+  const answersDiv = document.getElementById("answers");
+  answersDiv.innerHTML = "";
+  document.getElementById("errorText").innerText = "";
+
+  questions[questionIndex].answers.forEach(answer => {
+    const btn = document.createElement("button");
+    btn.innerText = answer.text;
+
+    btn.onclick = () => {
+      if (answer.correct) {
+        questionIndex++;
+        if (questionIndex < questions.length) {
+          loadQuestion();
+        } else {
+          nextSlide();
+        }
+      } else {
+        document.getElementById("errorText").innerText =
+          "Wrong. Try again.";
+      }
+    };
+
+    answersDiv.appendChild(btn);
+  });
 }
 
 function wrongAnswer() {
